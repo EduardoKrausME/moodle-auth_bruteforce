e19705f4d24226c57d29ad6b0baa9c41fdc3bcb0
Eduardo Kraus <kraus@eduardokraus.com>
2019-01-03 07:57:12 -0200

    First commit

---
+++ lang/en/auth_bruteforce.php
@@ -0,0 +1,29 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * lang file
+ *
+ * @package    auth_bruteforce
+ * @copyright  2019 Eduardo Kraus ME <kraus@eduardokraus.com>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['auth_bruteforcedescription'] = 'Brute Force system will block users after a retry limit.';
+$string['auth_bruteforcelimit'] = 'Maximum of attempts by IP to the environment block';
+$string['auth_bruteforcelimit_key'] = 'Maximum attempts per IP';
+$string['auth_bruteforcebloqued'] = 'You have exceeded the retry limit. Contact the Administrator for more details.';
+$string['pluginname'] = 'Brute Force Detect';
---
+++ lang/pt_br/auth_bruteforce.php
@@ -0,0 +1,29 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * lang file
+ *
+ * @package    auth_bruteforce
+ * @copyright  2019 Eduardo Kraus ME <kraus@eduardokraus.com>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['auth_bruteforcedescription'] = 'Sistema de Brute Force irá bloquear os usuários após um limite de tentativas.';
+$string['auth_bruteforcelimit'] = 'Limite máximo de tentativas por IP até o ambiente bloquear';
+$string['auth_bruteforcelimit_key'] = 'Máximo de tentativas por IP';
+$string['auth_bruteforcebloqued'] = 'Você excedeu o limite de tentativas. Contate o Administrador para mais detalhes.';
+$string['pluginname'] = 'Brute Force Detect';
